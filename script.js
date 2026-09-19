f55dde79d552eb5c9525dc17588ed0ee61000495
omarshooter <omarshooter3@gmail.com>
2026-09-19 20:07:48 +0300

    Redirect signup to homedesigns.ai

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const CONFIG = {
   sheetUrl: "",
   emailDest: "you@example.com",
   useFormSubmit: false,
-  redirectUrl: "https://www.google.com",
+  redirectUrl: "https://homedesigns.ai/",
 };
 
 const teams = [
